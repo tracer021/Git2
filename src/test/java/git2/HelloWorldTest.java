a5fef95daa1546233f32ba6784caeb5a3fe232bb
Fidel Guico <fguico@mssngsolutions.ph>
2015-03-09 00:25:00 +0800

    Completes  the greetings test

--- src/test/java/git2/HelloWorldTest.java
+++ src/test/java/git2/HelloWorldTest.java
@@ -2,9 +2,10 @@ package git2;
 
 public class HelloWorldTest {
 	
+	HelloWorld classUnderTest = new HelloWorld();
 	
 	public void greetingsShouldReturnHelloWorld( ){
-		
+		assertEquals("Hello World", classUnderTest.greetings());
 	}
 
 }
