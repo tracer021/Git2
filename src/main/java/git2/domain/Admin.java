e29745375bac4b30788558b25967df0ecda89f69
Fidel Guico <fguico@mssngsolutions.ph>
2015-03-09 23:14:29 +0800

    FEATURE2 : Add Admin entity

---
+++ src/main/java/git2/domain/Admin.java
@@ -0,0 +1,19 @@
+package git2.domain;
+
+public class Admin {
+	
+	private Long id; 
+	
+	private String userName;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+
+}
