1cb54afc1505b8e217e6bd9da19316863f230280
Fidel Guico <fguico@mssngsolutions.ph>
2015-03-09 23:12:17 +0800

    FEATURE1 Add Room Entity

---
+++ src/main/java/git2/domain/Room.java
@@ -0,0 +1,17 @@
+package git2.domain;
+
+public class Room {
+
+	private Long id;
+	
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+}
